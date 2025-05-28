78396147da01593a1835cafad6ba5a0a3ba89f3a
ed-techy <11414212+ed-techy@users.noreply.github.com>
2025-05-28 10:49:44 +0800

    add education and certificates

--- components/sections/Education.tsx
+++ components/sections/Education.tsx
@@ -6,19 +6,17 @@ export default function Education() {
           <div className='row'>
             <div className='col-lg-6 pt-3'>
               <div className='rounded-3 border border-1 position-relative h-100 overflow-hidden'>
-                <div className='box-linear-animation p-md-6 p-3 '>
+                <div className='box-linear-animation p-md-6 p-3 h-100'>
                   <div className='d-flex align-items-center'>
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
                       width={24}
                       height={28}
                       viewBox='0 0 24 28'
-                      fill='none'
                     >
                       <path
                         className='fill-primary-2'
                         d='M0 22.667V4.66699C0 2.45786 1.79087 0.666992 4 0.666992H22.6667C23.4031 0.666992 24 1.26395 24 2.00033V26.0003C24 26.7367 23.4031 27.3337 22.6667 27.3337H4.66667C2.08933 27.3337 0 25.2443 0 22.667ZM21.3333 24.667V20.667H4.66667C3.56209 20.667 2.66667 21.5625 2.66667 22.667C2.66667 23.7715 3.56209 24.667 4.66667 24.667H21.3333ZM9.33333 3.33366H4C3.26363 3.33366 2.66667 3.93062 2.66667 4.66699V18.4494C3.27284 18.1614 3.95093 18.0003 4.66667 18.0003H21.3333V3.33366H18.6667V14.0003L14 11.3337L9.33333 14.0003V3.33366Z'
-                        fill='#62A92B'
                       />
                     </svg>
                     <h2 className='mb-0 ms-2'>Education</h2>
@@ -28,53 +26,14 @@ export default function Education() {
                       <li className='position-relative z-1 mb-3'>
                         <div className='d-flex gap-2'>
                           <p className='text-300 text-nowrap fw-regular mb-0'>
-                            2020-2024:
-                          </p>
-                          <div>
-                            <span className='text-primary-2'>MIT</span>
-                            <p className='text-dark'>
-                              Bachelor’s Degree in Computer Science
-                            </p>
-                          </div>
-                        </div>
-                      </li>
-                      <li className='position-relative z-1 mb-3'>
-                        <div className='d-flex gap-2'>
-                          <p className='text-300 text-nowrap fw-regular mb-0'>
-                            2018-2019:
-                          </p>
-                          <div>
-                            <span className='text-primary-2'>Harvard University</span>
-                            <p className='text-dark'>
-                              Certification in React and Redux, Node.js Developer Course
-                            </p>
-                          </div>
-                        </div>
-                      </li>
-                      <li className='position-relative z-1 mb-3'>
-                        <div className='d-flex gap-2'>
-                          <p className='text-300 text-nowrap fw-regular mb-0'>
-                            2015-2016:
-                          </p>
-                          <div>
-                            <span className='text-primary-2'>Stanford University</span>
-                            <p className='text-dark'>
-                              Certification in Full Stack Web Development
-                            </p>
-                          </div>
-                        </div>
-                      </li>
-                      <li className='position-relative z-1 mb-3'>
-                        <div className='d-flex gap-2'>
-                          <p className='text-300 text-nowrap fw-regular mb-0'>
-                            2013-2015:
+                            2010-2014:
                           </p>
                           <div>
                             <span className='text-primary-2'>
-                              University of Washington
+                              Universidad Autónoma del Estado de México
                             </span>
                             <p className='text-dark'>
-                              Certification in React and Redux, Node.js Developer Course
+                              Bachelor’s Degree in Computer Science
                             </p>
                           </div>
                         </div>
@@ -87,70 +46,64 @@ export default function Education() {
               </div>
             </div>
             <div className='col-lg-6 pt-3'>
-              <div className='bg-3 rounded-3 border border-1 p-md-6 p-3 position-relative h-100 overflow-hidden'>
-                <div className='d-flex align-items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width={32}
-                    height={32}
-                    viewBox='0 0 32 32'
-                    fill='none'
-                  >
-                    <path
-                      className='fill-primary-2'
-                      d='M20 4H28V10.6667H25.3333V6.66667H20V4ZM12 4V6.66667H6.66667V10.6667H4V4H12ZM20 28V25.3333H25.3333V21.3333H28V28H20ZM12 28H4V21.3333H6.66667V25.3333H12V28ZM4 14.6667H28V17.3333H4V14.6667Z'
-                      fill='#A8FF53'
-                    />
-                  </svg>
-                  <h2 className='mb-0 ms-2'>Researched</h2>
+              <div className='bg-3 rounded-3 border border-1 position-relative h-100 overflow-hidden'>
+                <div className='box-linear-animation p-md-6 p-3'>
+                  <div className='d-flex align-items-center'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='-11 0 533 512'
+                      width={30}
+                      height={30}
+                    >
+                      <path
+                        className='fill-primary-2'
+                        d='M448 464c8.7998 0 15.999 -7.2002 15.999 -16v-288h-80c-17.7002 0 -32 -14.2998 -32 -32v-80h-160c-8.7998 0 -16 7.2002 -16 16v67.5c-13 -4.90039 -27.7002 -5 -41.2998 0.5c-2.59961 1 -5.09961 2.2998 -6.59961 3l-0.0996094 0.0996094v-71.0996 c0 -35.2998 28.7002 -64 64 -64h165.5c17 0 33.2002 6.7002 45.2002 18.7002l90.5996 90.5c12 12 18.7002 28.2998 18.7002 45.2998v293.5c0 35.2998 -28.7002 64 -64 64h-226.7c1.7002 -5.09961 2.7002 -10.5 2.7002 -16v-32h224zM109.199 161.6l-0.100586 -0.0996094 l15.7998 6.5c2 0.799805 4.2002 0.799805 6.10059 0l16 -6.40039c10 -4.09961 21.5 -1 28.0996 7.5l10.5 13.5c1.2002 1.59961 3.10059 2.69922 5.2002 2.99902l16.9004 2.2998c10.5996 1.40039 19 9.80078 20.5 20.501l2.2998 16.9004c0.299805 2 1.2998 3.90039 3 5.2002 l13.5 10.5c8.5 6.59961 11.5996 18.0996 7.5 28.0996l-6.5 15.7998c-0.799805 2 -0.799805 4.2002 0 6.10059l6.2998 15.7998c4.09961 10 1 21.5 -7.5 28.0996l-13.5 10.5c-1.59961 1.2002 -2.69922 3.10059 -2.99902 5.2002l-2.2998 16.9004 c-1.40039 10.7002 -9.80078 19.1006 -20.501 20.6006l-15.5 2.09961v105.8c0 5.90039 -3.2998 11.3008 -8.5 14.1006s-11.5 2.5 -16.4004 -0.799805l-39.0996 -26.0996l-39.0996 26.0996c-4.90039 3.2998 -11.1006 3.59961 -16.4004 0.799805s-8.5 -8.2002 -8.5 -14.1006 v-105.8l-15.5996 -2.09961c-10.5996 -1.5 -19 -9.90039 -20.5 -20.6006l-2.2998 -16.9004c-0.299805 -2 -1.2998 -3.90039 -3 -5.2002l-13.5 -10.5c-8.5 -6.59961 -11.5996 -18.0996 -7.5 -28.0996l6.40039 -15.7002c0.799805 -2 0.799805 -4.2002 0 -6.10059 l-6.40039 -15.7998c-4.09961 -10 -1 -21.5 7.5 -28.0996l13.5996 -10.5996c1.59961 -1.2002 2.69922 -3.10059 2.99902 -5.2002l2.2998 -16.9004c1.40039 -10.5996 9.80078 -19 20.501 -20.5l16.9004 -2.2998c2 -0.299805 3.90039 -1.2998 5.2002 -3l10.5 -13.5 c6.59961 -8.5 18.0996 -11.5996 28.0996 -7.5zM63.998 288c0 22.8652 12.1982 43.9932 32 55.4258s44.1982 11.4326 64 0s32 -32.5605 32 -55.4258s-12.1982 -43.9932 -32 -55.4258s-44.1982 -11.4326 -64 0s-32 32.5605 -32 55.4258z'
+                      />
+                    </svg>
+                    <h2 className='mb-0 ms-2'>Certificates</h2>
+                  </div>
+                  <div className='d-flex flex-column h-100 position-relative mt-5'>
+                    <ul className='ps-3'>
+                      <li className='position-relative z-1 mb-3'>
+                        <div className='d-flex gap-2'>
+                          <div>
+                            <span className='text-primary-2'>React JS Developer SME</span>
+                            <p className='text-dark'>
+                              Certified expertise in building dynamic web apps with
+                              React.js.
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                      <li className='position-relative z-1 mb-3'>
+                        <div className='d-flex gap-2'>
+                          <div>
+                            <span className='text-primary-2'>
+                              Android Apps Developer SME
+                            </span>
+                            <p className='text-dark'>
+                              Validated skills in developing native Android applications.
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                      <li className='position-relative z-1 mb-3'>
+                        <div className='d-flex gap-2'>
+                          <div>
+                            <span className='text-primary-2'>
+                              Android Security Essentials SME
+                            </span>
+                            <p className='text-dark'>
+                              Proven knowledge of core Android app security practices.
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                    <div className='line-left position-absolute top-0 border-start z-0' />
+                  </div>
+                  <div className='bg-overlay position-absolute bottom-0 start-0 z-1' />
                 </div>
-                <div className='d-flex flex-column h-100 position-relative mt-5'>
-                  <ul className='ps-3'>
-                    <li className='position-relative z-1 mb-3'>
-                      <div className='d-flex gap-2'>
-                        <p className='text-300 text-nowrap fw-regular mb-0'>2023-2024:</p>
-                        <div>
-                          <span className='text-primary-2'>
-                            Advanced Data Analytics with Big Data Tools
-                          </span>
-                          <p className='text-dark'>
-                            Utilized big data tools for advanced analytics and insights.
-                          </p>
-                        </div>
-                      </div>
-                    </li>
-                    <li className='position-relative z-1 mb-3'>
-                      <div className='d-flex gap-2'>
-                        <p className='text-300 text-nowrap fw-regular mb-0'>2021-2013:</p>
-                        <div>
-                          <span className='text-primary-2'>
-                            Cloud-Native Application Architectures
-                          </span>
-                          <p className='text-dark'>
-                            Studied best practices for designing cloud-native
-                            applications.
-                          </p>
-                        </div>
-                      </div>
-                    </li>
-                    <li className='position-relative z-1 mb-3'>
-                      <div className='d-flex gap-2'>
-                        <p className='text-300 text-nowrap fw-regular mb-0'>2019-2020:</p>
-                        <div>
-                          <span className='text-primary-2'>
-                            AI-Driven User Experience Personalization
-                          </span>
-                          <p className='text-dark'>
-                            Leveraged AI to personalize user experiences based on
-                            behavior.
-                          </p>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                  <div className='line-left position-absolute top-0 border-start z-0' />
-                </div>
-                <div className='bg-overlay position-absolute bottom-0 start-0 z-1' />
               </div>
             </div>
           </div>
